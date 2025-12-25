566077879094028c9b4dc902972c413e0693d3e1
Deepak Methala <deepakmethala123@gmail.com>
2025-12-25 16:47:29 +0530

    models user and video

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+import app from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config({
     path:"./env"
 });
-connectDB();
+connectDB()
+.then(()=>{
+    const port  = process.env.PORT || 8000
+    app.listen(port,()=>{
+        console.log(`Server is running on port ${port}`);       
+    })
+})
+.catch((err)=>{
+    console.log("DB CONNECTION FAILED :",err);
+})
